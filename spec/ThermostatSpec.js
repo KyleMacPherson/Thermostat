b30153f76d7d3e03051f9d2bf23f0c685aa55fab
MoeSadoon <msadoon590@gmail.com>
2015-09-29 16:26:44 +0100

    refactored tests

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,42 +1,40 @@
 describe("thermostat", function() {
 
-  it('starts at 20 degrees', function() {
+  beforeEach(function() {
     thermostat = new Thermostat;
+  });
+
+
+  it('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20)
   });
 
   it('can increase temperature', function() {
-    thermostat = new Thermostat;
     thermostat.tempUp(3);
     expect(thermostat.temperature).toEqual(23)
   });
 
   it('can decrease temperature', function() {
-    thermostat = new Thermostat;
     thermostat.tempDown(3);
     expect(thermostat.temperature).toEqual(17)
   });
 
   it('has a minimum temperature of 10 degrees', function() {
-    thermostat = new Thermostat;
     thermostat.tempDown(50);
     expect(thermostat.temperature).toEqual(10)
   });
 
   it('has power mode switched on by default', function() {
-    thermostat = new Thermostat;
     expect(thermostat.powerModeON).toBe(true)
   });
 
   it('can switch power mode off', function() {
-    thermostat = new Thermostat;
     thermostat.switchPowermode();
     expect(thermostat.powerModeON).toBe(false);
   });
 
 
   it('can switch power mode on',function(){
-    thermostat = new Thermostat
     thermostat.switchPowermode();
     thermostat.switchPowermode();
     expect(thermostat.powerModeON).toBe(true);
@@ -44,28 +42,24 @@ describe("thermostat", function() {
 
 
   it('cannot have temp higher than 25 when power mode ON',function() {
-    thermostat = new Thermostat;
     thermostat.tempUp(50);
     expect(thermostat.temperature).toEqual(25)
   });
 
 
   it('cannot have temp higher than 32 when power mode OFF', function() {
-    thermostat = new Thermostat;
     thermostat.switchPowermode();
     thermostat.tempUp(50);
     expect(thermostat.temperature).toEqual(32)
   });
 
   it('resets to 20',function() {
-    thermostat = new Thermostat;
     thermostat.tempUp(5);
     thermostat.reset();
     expect(thermostat.temperature).toEqual(20)
   });
 
   it('does not stay above 25 if power saving mode switch on', function() {
-    thermostat = new Thermostat;
     thermostat.switchPowermode();
     thermostat.tempUp(12);
     thermostat.switchPowermode();
