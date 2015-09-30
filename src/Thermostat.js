5146d021d33572767f1e25c896cbf9e136497ac3
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-30 07:36:43 +0100

    Basic thermostat working in browser

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,7 @@
 function Thermostat() {
   this.temperature = 20
   this.powerModeON = true
+  this.updateTemp()
 };
 
 Thermostat.prototype.tempUp = function(num) {
@@ -11,6 +12,7 @@ Thermostat.prototype.tempUp = function(num) {
   else if (!this.powerModeON && this.temperature > 32) {
     this.temperature = 32;
   }
+  this.updateTemp()
 };
 
 Thermostat.prototype.tempDown = function(num) {
@@ -18,10 +20,12 @@ Thermostat.prototype.tempDown = function(num) {
   if(this.temperature < 10){
     this.temperature = 10;
   }
+  this.updateTemp()
 };
 
 Thermostat.prototype.reset = function() {
   this.temperature = 20;
+  this.updateTemp()
 };
 
 Thermostat.prototype.switchPowermode = function() {
@@ -29,6 +33,7 @@ Thermostat.prototype.switchPowermode = function() {
   if (this.tooHot()) {
     this.temperature = 25;
   }
+  this.updateTemp()
 };
 
 
@@ -37,3 +42,17 @@ Thermostat.prototype.tooHot = function () {
     return true
   }
 };
+
+Thermostat.prototype.updateTemp = function () {
+    window.document.getElementById('temperature').innerHTML = this.temperature;
+    if(this.temperature < 18) {
+      window.document.getElementById('temperature').style.color = 'green'
+    } else if (this.temperature < 25) {
+      window.document.getElementById('temperature').style.color = 'yellow'
+    } else {
+      window.document.getElementById('temperature').style.color = 'red'
+    }
+
+};
+
+t = new Thermostat()
